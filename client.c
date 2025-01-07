46131aea211f01550cf71aff21c748e1905b969d
Branislav Blazek <blazek@pap.sk>
2025-01-07 10:32:33 +0100

    connect clients works

--- client.c
+++ client.c
@@ -1,45 +1,82 @@
-#include "pipe.h"
 #include "config.h"
+#include "pipe.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int connect_server(int* fd_read, int* fd_write, const char* pipe_path) {
-  printf("gooo\n");
-  *fd_read = pipe_open_read(pipe_path);
-  *fd_write = pipe_open_write(pipe_path);
-  printf("%d %d", *fd_read, *fd_write);  
-  if (*fd_read >= 0 && *fd_write >= 0) {
-    return 1;
+typedef struct {
+  int fd_fifo_handshake_read;
+  int fd_fifo_handshake_write;
+  int fd_fifo_server_write;
+  int fd_fifo_server_read;
+} fd_fifo_client_struct;
+
+void open_handshake(fd_fifo_client_struct *ffc) {
+  ffc->fd_fifo_handshake_write = pipe_open_write(PIPE_HANDSHAKE_CLIENT_SERVER);
+  ffc->fd_fifo_handshake_read = pipe_open_read(PIPE_HANDSHAKE_SERVER_CLIENT);
+}
+
+void connect_server(fd_fifo_client_struct *ffc) {
+  int client_id;
+
+  write(ffc->fd_fifo_handshake_write, "CONNECT", strlen("CONNECT"));
+
+  read(ffc->fd_fifo_handshake_read, &client_id, sizeof(client_id));
+  printf("Connected as Client %d.\n", client_id);
+
+  if (client_id == 1) {
+    ffc->fd_fifo_server_write = pipe_open_write(PIPE_CLIENT_1_SERVER);
+    ffc->fd_fifo_server_read = pipe_open_read(PIPE_SERVER_CLIENT_1);
+  } else if (client_id == 2) {
+    ffc->fd_fifo_server_write = pipe_open_write(PIPE_CLIENT_2_SERVER);
+    ffc->fd_fifo_server_read = pipe_open_read(PIPE_SERVER_CLIENT_2);
   }
+}
 
-  return 0;
+void close(fd_fifo_client_struct *ffc) {
+  pipe_close(ffc->fd_fifo_server_write);
+  pipe_close(ffc->fd_fifo_server_read);
+  pipe_close(ffc->fd_fifo_handshake_write);
+  pipe_close(ffc->fd_fifo_handshake_read);
 }
 
 void run_client() {
-  puts("Running client...");
-  int fd_pipe_read, fd_pipe_write;
-  int client_id, is_connected;
+  fd_fifo_client_struct ffc = { -1, -1, -1, -1 };
 
-  fd_pipe_read = pipe_open_read(PIPE_CLIENT_1);
-  fd_pipe_write = pipe_open_write(PIPE_CLIENT_1);
-  //is_connected = connect_server(&fd_pipe_read, &fd_pipe_write, PIPE_CLIENT_1);
-  //printf("%d\n", is_connected);
-  printf("%d %d\n", fd_pipe_read, fd_pipe_write);
-  return;
-  if (is_connected == 1) {
-    client_id = 1;
-  } else {
-    is_connected = connect_server(&fd_pipe_read, &fd_pipe_write, PIPE_CLIENT_2);
-    
-    if (is_connected == 1) {
-      client_id = 2;
-    } else {  
-      printf("Server is full!\n");
-      exit(EXIT_FAILURE);
-    }
+  open_handshake(&ffc);
+
+  if (ffc.fd_fifo_handshake_write == -1 || ffc.fd_fifo_handshake_read == -1) {
+    perror("Error with connection to FIFO pipelines");
+    exit(1);
   }
 
-  printf("Current client is #%d\n", client_id);
+  connect_server(&ffc);
+
+  if (ffc.fd_fifo_server_write == -1 || ffc.fd_fifo_server_read == -1) {
+    perror("Error can't open FIFO pipelines");
+    exit(1);
+  }
+
+  int num;
+  while (1) {
+      printf("Zadajte číslo na odoslanie: ");
+      scanf("%d", &num);
+
+      if (write(ffc.fd_fifo_server_write, &num, sizeof(int)) == -1) {
+          perror("Chyba pri odoslaní čísla");
+          break;
+      }
+
+      if (read(ffc.fd_fifo_server_read, &num, sizeof(int)) > 0) {
+          printf("Prijaté číslo: %d\n", num);
+      } else {
+          printf("Spojenie so serverom prerušené.\n");
+          break;
+      }
+  }
+
+  close(&ffc);
 }
--- server.c
+++ server.c
@@ -1,71 +1,114 @@
-#include "server.h"
-#include "pipe.h"
 #include "config.h"
+#include "pipe.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 typedef struct {
-  int client_1_connected, client_2_connected;
-  int fd_pipe_server;
-  int fd_pipe_client_1_read, fd_pipe_client_1_write;
-  int fd_pipe_client_2_read, fd_pipe_client_2_write;
-} fd_pipes_struct;
+  int fd_fifo_handshake_read;
+  int fd_fifo_handshake_write;
+  int fd_fifo_client_1_read;
+  int fd_fifo_client_1_write;
+  int fd_fifo_client_2_read;
+  int fd_fifo_client_2_write;
+} fd_fifo_server_struct;
 
 void init() {
-  pipe_init(PIPE_SERVER);
-  pipe_init(PIPE_CLIENT_1);
-  pipe_init(PIPE_CLIENT_2);
+  pipe_init(PIPE_HANDSHAKE_CLIENT_SERVER);
+  pipe_init(PIPE_HANDSHAKE_SERVER_CLIENT);
+  pipe_init(PIPE_CLIENT_1_SERVER);
+  pipe_init(PIPE_SERVER_CLIENT_1);
+  pipe_init(PIPE_CLIENT_2_SERVER);
+  pipe_init(PIPE_SERVER_CLIENT_2);
 }
 
-void connect_client(int* connected, int* fd_read, int*fd_write, const char* pipe_path) {
-  if (!*connected) {
-    puts("now trying to connect client");
-    *fd_read = pipe_open_read(pipe_path);
-    *fd_write = pipe_open_write(pipe_path);
-    
-    if (*fd_read >= 0 && *fd_write >= 0) {
-      *connected = 1;
-      printf("Successfully connected client with PIPE PATH = %s\n", pipe_path);
+void destroy() {
+  pipe_destroy(PIPE_HANDSHAKE_CLIENT_SERVER);
+  pipe_destroy(PIPE_HANDSHAKE_SERVER_CLIENT);
+  pipe_destroy(PIPE_CLIENT_1_SERVER);
+  pipe_destroy(PIPE_SERVER_CLIENT_1);
+  pipe_destroy(PIPE_CLIENT_2_SERVER);
+  pipe_destroy(PIPE_SERVER_CLIENT_2);
+}
+
+void open_handshake(fd_fifo_server_struct *ffs) {
+  ffs->fd_fifo_handshake_read = pipe_open_read(PIPE_HANDSHAKE_CLIENT_SERVER);
+  ffs->fd_fifo_handshake_write = pipe_open_write(PIPE_HANDSHAKE_SERVER_CLIENT);
+}
+
+void connect_clients(fd_fifo_server_struct *ffs) {
+  int client_count = 0;
+
+  while (client_count < 2) {
+    char buffer[16];
+    int bytes_read = read(ffs->fd_fifo_handshake_read, buffer, sizeof(buffer) - 1);
+
+    if (bytes_read > 0) {
+      buffer[bytes_read] = '\0';
+      if (strcmp(buffer, "CONNECT") == 0) {
+        client_count++;
+        write(ffs->fd_fifo_handshake_write, &client_count, sizeof(client_count));
+
+        if (client_count == 1) {
+          ffs->fd_fifo_client_1_read = pipe_open_read(PIPE_CLIENT_1_SERVER);
+          ffs->fd_fifo_client_1_write = pipe_open_write(PIPE_SERVER_CLIENT_1);
+          printf("Client 1 connected.\n");
+        } else if (client_count == 2) {
+          ffs->fd_fifo_client_2_read = pipe_open_read(PIPE_CLIENT_2_SERVER);
+          ffs->fd_fifo_client_2_write = pipe_open_write(PIPE_SERVER_CLIENT_2);
+          printf("Client 2 connected.\n");
+        }
+      }
     }
   }
 }
 
-void connect_clients(fd_pipes_struct* fd_pipes) {
-  connect_client(
-    &fd_pipes->client_1_connected, 
-    &fd_pipes->fd_pipe_client_1_read,
-    &fd_pipes->fd_pipe_client_1_write,
-    PIPE_CLIENT_1);
-  
-  connect_client(
-    &fd_pipes->client_2_connected, 
-    &fd_pipes->fd_pipe_client_2_read,
-    &fd_pipes->fd_pipe_client_2_write,
-    PIPE_CLIENT_2);
-} 
-
-void close(fd_pipes_struct* fd_pipes) {
-  pipe_close(fd_pipes->fd_pipe_server);
-  pipe_close(fd_pipes->fd_pipe_client_1_read);
-  pipe_close(fd_pipes->fd_pipe_client_1_write);
-  pipe_close(fd_pipes->fd_pipe_client_2_read);
-  pipe_close(fd_pipes->fd_pipe_client_2_write);
+void close(fd_fifo_server_struct *ffs) {
+  pipe_close(ffs->fd_fifo_client_1_read);
+  pipe_close(ffs->fd_fifo_client_1_write);
+  pipe_close(ffs->fd_fifo_client_2_read);
+  pipe_close(ffs->fd_fifo_client_2_write);
+  pipe_close(ffs->fd_fifo_handshake_read);
+  pipe_close(ffs->fd_fifo_handshake_write);
 }
 
 void run_server() {
-  fd_pipes_struct fd_pipes;
-  
-  init();  
-  
-  //fd_pipes.fd_pipe_server = pipe_open_write(PIPE_SERVER);
+fd_fifo_server_struct ffs = { -1, -1, -1, -1, -1, -1 };
 
+init();
+
+printf("Server up. Waiting for clients...\n");
+
+open_handshake(&ffs);
+
+if (ffs.fd_fifo_handshake_read == -1 || ffs.fd_fifo_handshake_write == -1) {
+  perror("Error can't open FIFO pipelines");
+  exit(1);
+}
+
+connect_clients(&ffs);
+
+printf("Both clients connected. Starting communicaton...\n");
+
+  int num;
   while (1) {
-    connect_clients(&fd_pipes);
+      // Čítanie od klienta 1 a posielanie klientovi 2
+      if (read(ffs.fd_fifo_client_1_read, &num, sizeof(int)) > 0) {
+          printf("Od klienta 1 prijaté číslo: %d\n", num);
+          write(ffs.fd_fifo_client_2_write, &num, sizeof(int));
+      }
+
+      // Čítanie od klienta 2 a posielanie klientovi 1
+      if (read(ffs.fd_fifo_client_2_read, &num, sizeof(int)) > 0) {
+          printf("Od klienta 2 prijaté číslo: %d\n", num);
+          write(ffs.fd_fifo_client_1_write, &num, sizeof(int));
+      }
   }
 
-  close(&fd_pipes);
+  close(&ffs);
 
-  pipe_destroy(PIPE_SERVER);
+  destroy();
 }
